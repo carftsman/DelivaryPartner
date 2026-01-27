5d2f2fd68258e8cc7cd95a738c7eea1f55d1ce5b
ROHITSAIR <rohitravipat900@gmail.com>
2026-01-27 16:48:48 +0530

    modified orderCreate and confirm

--- server.js
+++ server.js
@@ -56,12 +56,8 @@ connectDB();
 const server = http.createServer(app);
  
 // Attach WebSocket to SAME server
-<<<<<<< Updated upstream
 initWebSocket(server);  
 // initRiderSocket(server);
-=======
-initWebSocket(server);
->>>>>>> Stashed changes
  
 const PORT = process.env.PORT || 4000;
  
